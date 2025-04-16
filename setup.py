f383f3818cf317e2ff75fe2ca6e6f29e6ec1fecf
Raphael Elias <thefael98@gmail.com>
2025-04-16 15:12:16 -0300

    feat: new version of lazyreadme for 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lazyreadme",
-    version="1.0.0",
+    version="1.0.1",
     description="Gerador automático de README para seus projetos",
     author="Seu Nome",
     author_email="seu.email@exemplo.com",
